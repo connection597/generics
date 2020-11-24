d2b098170b7b43ef3dba2fc0809e83d1b13438ed
connection579 <wlswn4118@naver.com>
2020-11-24 14:58:09 +0900

    제너릭 클래스 사용하기

---
+++ src/generics/Plastic.java
@@ -0,0 +1,11 @@
+package generics;
+
+public class Plastic {
+	public void doPrinting() {
+		System.out.println("Plastic재료를 출력합니다");
+	}
+	public String toString() {
+		return "채료는 Plastic입니다";
+	}
+
+}
